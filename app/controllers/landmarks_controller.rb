46ada5c5190d31f867fd3a32c798ddc68e6cc5e4
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-18 18:51:04 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -24,7 +24,9 @@ class LandmarksController < ApplicationController
     @landmark = Landmark.find(params[:id])
     @landmark.name = params['landmark']['name']
     @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
     
+    redirect to '/landmarks/#{'
   end 
   
 end
