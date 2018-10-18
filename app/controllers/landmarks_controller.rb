1d227694c25dc9f2390c634374174fd881dbe8e2
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-18 18:50:33 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -22,7 +22,8 @@ class LandmarksController < ApplicationController
   
   post '/landmarks/:id' do 
     @landmark = Landmark.find(params[:id])
-    @landmark.name = params[:landmark]
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params[]
   end 
   
 end
