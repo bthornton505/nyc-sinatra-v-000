44ee025a5cbe234f6e265b0682a714177ecb9358
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-18 18:52:45 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -30,7 +30,7 @@ class LandmarksController < ApplicationController
   end 
   
   post '/landmarks' do 
-    @landmark = Landmark.create(name: params)
+    @landmark = Landmark.create(name: params['landmark']['name'], year_completed: params[])
   end 
   
 end
