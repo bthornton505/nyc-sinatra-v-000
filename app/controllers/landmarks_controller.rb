20cc84a94d6db4d6ef01b0aff29c45db8b297926
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-18 18:23:28 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,13 +6,13 @@ class LandmarksController < ApplicationController
     erb :'landmarks/index'
   end 
   
-  get '/landmarks/new' do 
-    erb :'landmarks/new'
-  end 
-  
   get '/landmarks/:id' do 
     @landmark = Landmark.find(params[:id])
     erb :'landmarks/show'
+  end 
+  
+  get '/landmarks/new' do 
+    erb :'landmarks/new'
   end 
   
   # post '/landmarks' do 
